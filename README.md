98f19aa3f5a46b0d58239967cb537403dd86e05a
Cissy Xia <mail@cissyxia.com>
2014-08-23 13:40:42 +0800

    Refine the script
    1. use better descriptive column name
    2. simplify the script

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 Getting-and-Cleaning-Data
 =========================
+
+
+# How the script works?
+ 1. The script should put in the same working directory with the Samsung data.
+ 2. Reads the meta data which contians label name, column name, activity first and put it aside.
+ 3. Reads the data from the txt file, merges the training and the test sets to create one data set 'data_original'.
+ 4. Extracts only the measurements on the mean and standard deviation for each measurement by grep the text string 'mean()' and 'std()' from the column name, the results will be put in the data set 'dataset_mean'.
+ 5. Label the dataset, clear up '()' and '-'.
+ 6. Creates a second, independent tidy data set 'data_average'.
+ 7. Find the all rows for each activity and each subject, and use 'colMeans' to get the average number for each column, put them in the data set 'data_average'.
+ 8. Merge the 'data_average' and 'tmp_activity' in order to use descriptive activity names in the data set. To use 'colMeans' properly in the previous step, we'd better put this step at last. 
+ 9. Output 'data_average' to txt file 'tidydataset.txt' by using 'write.table()' and 'row.name=FALSE'.
+
+
+
+# Code book
+
+**activity_id**
+ * Class: Integer 
+ * Description: An identifier of the activity name that the subject did in the experiment.
+ ** 1 WALKING
+ ** 2 WALKING_UPSTAIRS
+ ** 3 WALKING_DOWNSTAIRS
+ ** 4 SITTING
+ ** 5 STANDING
+ ** 6 LAYING
+
+
+**activity_label**
+ * Class: Character 
+ * Description: Activity name.
+
+ 
+**subject_id**
+ * Class: Integer 
+ * Description: An identifier of the subject who carried out the experiment.
+
+ 
+**time_body_accelerometer_mean_xyz**
+ * Class: Numeric
+ * Description: 
+ ** time domain signals were captured at a constant rate of 50 Hz.
+ ** mean value
+ ** body acceleration signals
+ ** accelerometer 3-axial raw signals 
+ ** in the X, Y and Z directions
+
+ 
+**time_body_accelerometer_standard_deviation_xyz**
+ * Class: Numeric
+ * Description:
+ ** time domain signals were captured at a constant rate of 50 Hz.
+ ** standard deviation
+ ** body acceleration signals
+ ** accelerometer 3-axial raw signals 
+ ** in the X, Y and Z directions
+ 
+ 
--- run_analysis.R
+++ run_analysis.R
@@ -1,69 +1,62 @@
 
 
-## Get meta data: label, column name.
+## ## ## Get meta data: label, column name.
 tmp_features <- tolower(read.table('features.txt')[,2])
 tmp_activity <- read.table('activity_labels.txt',col.names=c('activity_id','activity_label'),colClasses=c('integer','character'))
-tmp_activity$activity_label <- tolower(tmp_activity$activity_label)
-tmp_columns <- c('subject_id','activity_id',tmp_features)
+tmp_columns_name <- c('subject_id','activity_id',tmp_features) # set column name
 
-## Read the data from the txt file
-tmp_subject_test <- read.table('test/subject_test.txt')
-tmp_x_test <- read.table('test/X_test.txt')
-tmp_y_test <- read.table('test/y_test.txt')
-tmp_test <- cbind(tmp_subject_test, tmp_y_test, tmp_x_test)
-names(tmp_test) <- tmp_columns
-rm(tmp_subject_test, tmp_x_test, tmp_y_test)
-
-tmp_subject_train <- read.table('train/subject_train.txt')
-tmp_x_train <- read.table('train/X_train.txt')
-tmp_y_train <- read.table('train/y_train.txt')
-tmp_train <- cbind(tmp_subject_train, tmp_y_train, tmp_x_train)
-names(tmp_train) <- tmp_columns
-rm(tmp_subject_train, tmp_x_train, tmp_y_train)
+## ## ## Read the data from the txt file
+tmp_test <- cbind(read.table('test/subject_test.txt'), read.table('test/y_test.txt'), read.table('test/X_test.txt'))
+tmp_train <- cbind(read.table('train/subject_train.txt'), read.table('train/y_train.txt'), read.table('train/X_train.txt'))
 
 ## Merges the training and the test sets to create one data set.
 dataset_original <- rbind(tmp_test, tmp_train, deparse.level = 1)
-rm(tmp_test, tmp_train)
+names(dataset_original) <- tmp_columns_name
+
+rm(tmp_test, tmp_train) # clear up temp dt
 
 ## Extracts only the measurements on the mean and standard deviation for each measurement. 
-dataset_mean <- dataset_original[(c(1,2,grep('mean\\(\\)*|std',tmp_columns)))]
+dataset_mean <- dataset_original[(c(1,2,grep('mean\\(\\)*|std\\(\\)',tmp_columns_name)))]
         
-## Uses descriptive activity names to name the activities in the data set.
-##dataset_mean <- merge(tmp_activity, dataset_mean, by.x='activity_id')
-
 ## appropriately labels the data set with descriptive variable names
-names(dataset_mean) <- gsub('\\(\\)','',names(dataset_mean))
-names(dataset_mean) <- gsub('bodybody','body',names(dataset_mean))
-names(dataset_mean) <- gsub('-','_',names(dataset_mean))
+## ## split the name and get clean name
+columns_name <- names(dataset_mean)
+columns_name <- gsub('-','_',columns_name)
+columns_name <- gsub('bodybody','body',columns_name)
+columns_name <- gsub('body','_body_',columns_name)
+columns_name <- gsub('gravity','_gravity_',columns_name)
+columns_name <- gsub('mag_','_mag_',columns_name)
+columns_name <- gsub('jerk_','_jerk_',columns_name)
+## ## replace the arrbiriaiont with descriptive names
+columns_name <- gsub('\\(\\)','',columns_name)
+columns_name <- gsub('^t_','time_',columns_name)
+columns_name <- gsub('^f_','frequency_',columns_name)
+columns_name <- gsub('_std','_standard_deviation',columns_name)
+columns_name <- gsub('_mag_','_magnitude_',columns_name)
+columns_name <- gsub('_gyro_','_gyroscope_',columns_name)
+columns_name <- gsub('_acc_','_accelerometer_',columns_name)
+## ## apply the new columns to the data set
+names(dataset_mean) <- columns_name
 
-## tidy data set with the average of each variable for each activity and each subject. 
-
- # get subject list
-subjects <- sort(unique(dataset_mean$subject_id))
- 
 ## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-
-## get each subject
-for (i in 1:length(subjects)) {
-        sid <- subjects[[i]]
-        for (j in 1:length(tmp_activity$activity_id)) {
+dataset_average <- data.frame()
+## get each subject, from 1 to the max subject id
+for (sid in 1:max(dataset_mean$subject_id)) {
+        for (aid in 1:max(tmp_activity$activity_id)) {
                 
                 ## get rows belong to each activity and each subject 
-                tmp_data <- dataset_mean[which(dataset_mean$subject_id == sid & dataset_mean$activity_id == j ),]
-                
+                tmp_data <- dataset_mean[which(dataset_mean$subject_id == sid & dataset_mean$activity_id == aid),]
+              
                 ## return the average of each variable
-                if (!exists('dataset_average')) {
-                        dataset_average <- t(colMeans(tmp_data))
-                } else {
+                if(nrow(tmp_data)>0){ ## in case some subject_id or activity_id is missing
                         dataset_average <- rbind(dataset_average, t(colMeans(tmp_data)))
                 }
                 
         }
 }
-rm(i,j,sid,subjects)
 
 ## Uses descriptive activity names to name the activities in the data set.
-dataset_average <- merge(tmp_activity, dataset_average, by.x='activity_id',sort = TRUE)
+dataset_average <- merge(tmp_activity, dataset_average, by.x='activity_id')
 
 ## Create tidy data set
 write.table(dataset_average[order(dataset_average$activity_id, dataset_average$subject_id),], 'tidydataset.txt', row.name=FALSE)
